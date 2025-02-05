dca125caef487a4318574bdb6f8b6ca71c9c9ffe
Srishti Tripathi <tripathisrishti.2010@gmail.com>
2025-02-05 14:09:27 +0530

    Update Card.javaby adding createDeck method

--- Card.java
+++ Card.java
@@ -12,3 +12,15 @@ class Card{
 		return rank+" of"+suit;
 	} 
 }
+	public void createDeck(){
+		String[] ranks = {"2" , "3" , "4", "5" , "6", "7", "8", "9", "10", "J", "Q", "K", "A"};
+		String[] suits = {"Spades" , "Hearts" , "Diamonds" ,"Clubs"};
+		
+		for (String rank : ranks){
+			for(String suit : suits){
+				deck.add(new Card(rank,suit));
+			}
+				
+		}
+			
+	}
