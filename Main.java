3a4b0eee341102c478682c1878ad3f043a1bfb8f
Srishti Tripathi <tripathisrishti.2010@gmail.com>
2025-02-19 13:47:58 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,1 +1,20 @@
+//Main.java
 
+import java.util.*;
+class Main{
+	public static void main(String args []){
+		Deck deck = new Deck();
+		System.out.println("_______________Create Deck _________________");
+		deck.createDeck();
+		System.out.println("_______________Print Deck _________________");
+		deck.displayDeck();
+		System.out.println("_______________Shuffle Deck _________________");
+		deck.shuffleDeck();
+		deck.displayDeck();
+		System.out.println("_______________Random Crads_________________");
+		deck.shuffleDeck();
+		deck.randomCards();
+		deck.displayDeck();
+	}
+	
+}
